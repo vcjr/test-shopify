e64d9870fb837fb1d95d7fd0bdfac3d6f614927d
Victor Crispin <victorrcrispinjr@gmail.com>
2021-03-24 15:02:27 -0400

    initial commit

---
+++ activate/pages/api/Products.js
@@ -0,0 +1,9 @@
+// Load the current session to get the `accessToken`.
+// const session = await Shopify.Utils.loadCurrentSession(req, res);
+// // Create a new client for the specified shop.
+// const client = new Shopify.Clients.Rest(session.shop, session.accessToken);
+// // Use `client.get` to request the specified Shopify REST API endpoint, in this case `products`.
+// const products = await client.get({
+//   path: 'products',
+// });
+
---
+++ activate/pages/index.js
@@ -0,0 +1,14 @@
+import { Heading, Page } from "@shopify/polaris";
+
+const Index = () => {
+ 
+
+  return (
+    <Page>
+      <Heading>You are now connected to Activate, Go to your profile on activate</Heading>
+
+    </Page>
+  )
+};
+
+export default Index;
